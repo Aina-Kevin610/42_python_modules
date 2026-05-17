9269ad58aecb5385994b6fa6b9fe5fea10fc9d90
Aina-Kevin610 <randrianambinintsoaainakevin@gmail.com>
2026-05-17 23:30:36 +0300

    feat:module04

--- module04/ex3/ft_vault_security.py
+++ module04/ex3/ft_vault_security.py
@@ -18,21 +18,16 @@ def secure_archive(filename, mode="read", content=None):
 
 
 if __name__ == "__main__":
-    print("=== Cyber Archives Security ===")
-    print()
-
+    print("=== Cyber Archives Security ===\n")
     print("Using 'secure_archive' to read from a nonexistent file:")
     print(secure_archive("/not/existing/file"))
-    print()
-
+    print("\n")
     print("Using 'secure_archive' to read from an inaccessible file:")
     print(secure_archive("/etc/shadow"))
-    print()
-
+    print("\n")
     print("Using 'secure_archive' to read from a regular file:")
-    print(secure_archive("archive.txt"))
-    print()
-
+    print(secure_archive("ancient_fragment.txt"))
+    print("\n")
     result = secure_archive("archive.txt")
     print("Using 'secure_archive' to write previous content to a new file:")
     print(secure_archive("new_archive.txt", mode="write", content=result[1]))
