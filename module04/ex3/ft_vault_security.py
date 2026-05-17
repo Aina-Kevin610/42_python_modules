90e29540ea606b4d61384c7cd038321845945849
Aina-Kevin610 <randrianambinintsoaainakevin@gmail.com>
2026-05-17 23:24:14 +0300

    feat

--- module04/ex3/ft_vault_security.py
+++ module04/ex3/ft_vault_security.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 
 
-def secure_archive(filename: str) -> tuple[bool, str]:
-    try:
-        with open(filename, "r") as file:
-    except FileExistsError as e:
-        return (False, e)
-    except PermissionError as e:
-        return (False, e)
-
-
-def main() -> None:
-
+def secure_archive(filename, mode="read", content=None):
+    if mode == "write":
+        try:
+            with open(filename, "w") as f:
+                f.write(content)
+            return (True, "Content successfully written to file")
+        except Exception as e:
+            return (False, str(e))
+    else:
+        try:
+            with open(filename, "r") as f:
+                return (True, f.read())
+        except Exception as e:
+            return (False, str(e))
 
 
 if __name__ == "__main__":
     print("=== Cyber Archives Security ===")
-    main()
+    print()
+
+    print("Using 'secure_archive' to read from a nonexistent file:")
+    print(secure_archive("/not/existing/file"))
+    print()
+
+    print("Using 'secure_archive' to read from an inaccessible file:")
+    print(secure_archive("/etc/shadow"))
+    print()
+
+    print("Using 'secure_archive' to read from a regular file:")
+    print(secure_archive("archive.txt"))
+    print()
+
+    result = secure_archive("archive.txt")
+    print("Using 'secure_archive' to write previous content to a new file:")
+    print(secure_archive("new_archive.txt", mode="write", content=result[1]))
