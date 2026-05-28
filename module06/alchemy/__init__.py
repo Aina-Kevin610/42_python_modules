3bad8925ddeef78564431158764510303d275e71
Aina-Kevin610 <randrianambinintsoaainakevin@gmail.com>
2026-05-28 19:28:31 +0300

    feat:module06

--- module06/alchemy/__init__.py
+++ module06/alchemy/__init__.py
@@ -1,14 +1,3 @@
 from .elements import create_air
 from .potion import healing_potion as heal, strength_potion
-
-
-'''
-__all__ = [
-    create_air,
-    create_earth,
-    create_fire,
-    create_water,
-    healing_potion,
-    strength_potion,
-]
-'''
+from .transmutation.recipes import lead_to_gold
---
+++ module06/ft_transmutation_1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import alchemy.transmutation
+
+
+if __name__ == "__main__":
+    print("=== Transmutation 1 ===")
+    print(alchemy.transmutation.lead_to_gold())
+    print()
