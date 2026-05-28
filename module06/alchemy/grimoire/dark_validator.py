646c55a64ada0012ea8857a6f668f98cd8c1001c
Aina-Kevin610 <randrianambinintsoaainakevin@gmail.com>
2026-05-28 22:05:06 +0300

    feat:module06

---
+++ module06/alchemy/grimoire/dark_validator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from .dark_spellbook import dark_spell_allowed_ingredients
+
+
+def validate_ingredients(ingredients: str) -> str:
+    allowed_ingredient: list[str] = light_spellbook_allowed_ingredients()
+
+    if ingredients in allowed_ingredient:
+        return f"{ingredients} - VALID"
+    else:
+        return f"{ingredients} - INVALID"
