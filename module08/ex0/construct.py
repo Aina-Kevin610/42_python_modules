500c52c0deb107e78bc479476cfc000a3d1840b9
Aina Kevin Randrianambinintsoa <airandri@c1r9s3.42antananarivo.mg>
2026-06-08 17:49:53 +0300

    feat

--- module08/ex0/construct.py
+++ module08/ex0/construct.py
@@ -1,20 +1,42 @@
 #!/usr/bin/env python3
 
 import os
+import site
+import sys
 
 
-try:
-    import flask
-        
-except ModuleNotFoundError as e:
-    print("Curent Python: ", os.fspath("/usr/bin/python3.11"))
+if sys.prefix == sys.base_prefix:
+    print("MATRIX STATUS: You're still plugged in")
 
-    print ("Virtual Environement: None detected")
-    print("WARNING: You're in a global environement!")
-    print("The machine can see everything you install.")
+    print()
+
+    print("Current python: ", sys.prefix)
+    print("Virtual Environment: None detected")
+
+    print()
+
+    print("WARNING: You're in the global environment!")
+    print("The machines can see everything you install.")
+
+    print()
 
     print("To enter the construct, run:")
     print("python -m venv matrix_env")
     print("source matrix_env/bin/activate # On Unix")
-    print("matrix_env\\Scripts\activate # On Windows")
+    print("matrix_env\\Scripts\\activate # On Windows")
+
+    print()
+
     print("Then run this program again.")
+else:
+    print("MATRIX STATUS: Welcome to the construct")
+
+    print()
+
+    print("Current python: ", sys.prefix)
+    print("Virtual Environment: None detected")
+
+    print()
+
+    print("WARNING: You're in the global environment!")
+    print("The machines can see everything you install.")
