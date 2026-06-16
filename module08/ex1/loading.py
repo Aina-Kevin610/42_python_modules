8f17104bb807848c461d4f7cab37ae270a162cdd
Aina Kevin Randrianambinintsoa <airandri@c1r11s2.42antananarivo.mg>
2026-06-16 17:31:19 +0300

    feat

--- module08/ex1/loading.py
+++ module08/ex1/loading.py
@@ -1,37 +1,34 @@
 #!/usr/bin/env python3
 
-try:
-    import importlib
-    import requests as rq
-    import numpy as np
-    import pandas as pd
-    import matplotlib.pyplot as plt
-except Exception as e:
-    print("Error - ", e)
+import importlib
 
 
 if __name__ == "__main__":
     print("\nLOADING STATUS: Loading programs...\n")
     print("Checking dependencies:")
+
     pkg = {
         "matplotlib" : "Visualization read", 
         "pandas" :  "Data manipulation ready",
         "numpy" : "Numerical computation ready",
         "requests" : "Network access ready",
     }
+
     try:
         for module, message in pkg.items():
             mod = importlib.import_module(module)
             print(f"[OK] {module} ({mod.__version__}) - {message}")
-    except ModuleNotFoundError as e:
-        print(f"[KO] {mod}) - import error")
 
-    print("\nAnalyzing Matrix data...")
-    data = np.random.randn(1000, 2)
-    data = pd.DataFrame(data)
-    print(data)
-    plt.plot(data)
-    plt.show()
+        print("\nAnalyzing Matrix data...")
+        np = importlib.import_module("numpy")
+        data = np.random.randn(1000, 1)
 
+        pd = importlib.import_module("pandas")
+        data = pd.DataFrame(data)
+        print(data)
 
-    
+        plt = importlib.import_module("matplotlib.pyplot")
+        plt.plot(data)
+        plt.show()
+    except ModuleNotFoundError:
+        print(f"[KO] ({module}) - import error")
