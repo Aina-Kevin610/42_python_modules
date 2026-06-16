bcb5e12859c4c1e79ed11e33e007e072dbdf572d
Aina Kevin Randrianambinintsoa <airandri@c1r11s2.42antananarivo.mg>
2026-06-16 17:47:13 +0300

    feat

--- module08/ex1/loading.py
+++ module08/ex1/loading.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
         "matplotlib" : "Visualization read", 
         "pandas" :  "Data manipulation ready",
         "numpy" : "Numerical computation ready",
-        "requests" : "Network access ready",
     }
 
     try:
@@ -21,14 +20,21 @@ if __name__ == "__main__":
 
         print("\nAnalyzing Matrix data...")
         np = importlib.import_module("numpy")
-        data = np.random.randn(1000, 1)
+        matrix = np.random.randn(1000, 2)
 
+        print(f"Processing {len(matrix)} data points...")
         pd = importlib.import_module("pandas")
-        data = pd.DataFrame(data)
-        print(data)
+        matrix_data = pd.DataFrame(matrix, columns=["x axe", "y axe"])
+        print(matrix_data)
 
         plt = importlib.import_module("matplotlib.pyplot")
-        plt.plot(data)
-        plt.show()
+        plt.plot(matrix)
+        print("Generating visualization...")
+        plt.savefig("matrix_analysis.png")
+
+        print("\nAnalysis complete!")
+        print("Results saved to: matrix_analysis.png")
     except ModuleNotFoundError:
         print(f"[KO] ({module}) - import error")
+    except Exception:
+        print("Error")
